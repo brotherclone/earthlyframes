4175f9f3f3ce5ce908def7ca71e83c0ede4e7ba4
Gabriel Walsh <gawalsh@adobe.com>
2015-10-15 17:48:38 -0400

    offer routes in place

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,28 +1,26 @@
 class ReleasesController < ApplicationController
   before_action :set_release, only: [:show, :edit, :update, :destroy]
 
-  # GET /releases
-  # GET /releases.json
   def index
     @releases = Release.all
   end
 
-  # GET /releases/1
-  # GET /releases/1.json
+  def offer
+    @releases = Release.all
+  end
+
   def show
   end
 
-  # GET /releases/new
+
   def new
     @release = Release.new
   end
 
-  # GET /releases/1/edit
+
   def edit
   end
 
-  # POST /releases
-  # POST /releases.json
   def create
     @release = Release.new(release_params)
 
@@ -37,8 +35,6 @@ class ReleasesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /releases/1
-  # PATCH/PUT /releases/1.json
   def update
     respond_to do |format|
       if @release.update(release_params)
@@ -51,8 +47,6 @@ class ReleasesController < ApplicationController
     end
   end
 
-  # DELETE /releases/1
-  # DELETE /releases/1.json
   def destroy
     @release.destroy
     respond_to do |format|
@@ -62,12 +56,11 @@ class ReleasesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_release
       @release = Release.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def release_params
       params.require(:release).permit(:title, :formatname, :price, :album_id)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,9 @@ Rails.application.routes.draw do
   get 'soon', to:'soon#index'
   get 'home', to:'home#index'
 
+  get 'offer_releases', to: 'releases#offer'
+  get 'offer_shows', to: 'shows#offer'
+  get 'offer_posts', to: 'posts#offer'
+
   root 'home#index'
 end
