f01689b9d893ee3a30abec60458f4922326049c4
Gabriel Walsh <brotherclone@gmail.com>
2016-05-13 06:42:50 -0400

    fixed drop

--- db/migrate/20151001131541_clean_ef.rb
+++ db/migrate/20151001131541_clean_ef.rb
@@ -1,11 +1,25 @@
 class CleanEf < ActiveRecord::Migration
-  def change
-    drop_table :albums
-    drop_table :albumtypes
-    drop_table :albumformats
-    drop_table :songs
-    drop_table :shows
-    drop_table :images
-    drop_table :posts
-  end
+  # def change
+  #   if ActiveRecord::Base.connection.table_exists? :albums
+  #     drop_table :albums
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :albumtypes
+  #     drop_table :albumtypes
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :albumformats
+  #     drop_table :albumformats
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :songs
+  #     drop_table :songs
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :shows
+  #     drop_table :shows
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :images
+  #     drop_table :images
+  #   end
+  #   if ActiveRecord::Base.connection.table_exists? :posts
+  #     drop_table :posts
+  #   end
+  # end
 end
