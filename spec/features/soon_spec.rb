123535c7c04e94ff8a7841c1f8222f466964326e
Gabriel Walsh <brotherclone@gmail.com>
2016-09-14 21:28:57 -0400

    lower case

--- spec/features/soon_spec.rb
+++ spec/features/soon_spec.rb
@@ -3,7 +3,7 @@ describe 'Coming Soon display correctly', :type => :feature do
   it 'checks the shows index page', :js => true  do
     visit soon_path
     expect(page).to have_css('h1.soon')
-    expect(page).to have_content('Back Shortly')
+    expect(page).to have_content('back shortly')
     expect(page).not_to have_css('navigation-centered')
     expect(page).not_to have_css('footer')
   end
