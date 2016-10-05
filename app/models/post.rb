29076322abc139dd0f3f683e1c04d219d6bd62fd
Gabriel Walsh <gawalsh@adobe.com>
2016-10-04 22:04:06 -0400

    Test, Next/Prev

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,9 @@ class Post < ActiveRecord::Base
   mount_uploader :inline_image, PhotosUploader
   validates :title, presence: true
   def next_post
-    Post.where('post_date > ?', post_date).order(post_date: :asc).first
+    Post.where('post_date > ? AND is_live = ?', post_date, is_live).order(post_date: :asc).first
   end
   def prev_post
-    Post.where('post_date < ?', post_date).order(post_date: :asc).first
+    Post.where('post_date < ? AND is_live = ?', post_date, is_live).order(post_date: :asc).first
   end
 end
