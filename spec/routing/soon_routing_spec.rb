357972adeb91ec453c32b68f21670ac3c7db4f44
Gabriel Walsh <gawalsh@adobe.com>
2016-10-11 14:17:29 -0400

    error in soon routing spec

--- spec/routing/soon_routing_spec.rb
+++ spec/routing/soon_routing_spec.rb
@@ -5,10 +5,8 @@ RSpec.describe SoonController, type: :routing do
     it 'routes to #index' do
       expect(:get => '/soon').to route_to('soon#index')
     end
-
     it 'checks that coming soon is no longer live.' do
-      if Rails.env.production?
-        expect(:get => '/').not_to route_to('soon#index')
+      expect(:get => '/').not_to route_to('soon#index')
      end
   end
 end
