ab497b28bf7befcd33ac22099367cb9f054ede4e
Gabriel Walsh <brotherclone@gmail.com>
2020-07-17 14:41:29 -0400

    basic devise user login added

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,18 @@
 FactoryBot.define do
   factory :user do
-    
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name   }
+    username {Faker::Internet.username}
+    email { Faker::Internet.email }
+    password { Faker::Internet.password }
+    encrypted_password { Faker::Internet.password }
+    reset_password_token { Faker::Internet.password }
+    reset_password_sent_at { Faker::Date.between(from: 2.years.ago, to:Date.today) }
+    remember_created_at { Faker::Date.between(from: 2.years.ago, to:Date.today) }
+    confirmation_token { Faker::Internet.password }
+    confirmation_sent_at { Faker::Date.between(from: 2.years.ago, to:Date.today) }
+    unconfirmed_email { Faker:: Internet.email }
+    failed_attempts { Faker::Number.number(digits: 1) }
+    unlock_token { Faker::Internet.password }
   end
 end
--- spec/requests/pulsar_request_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe "Pulsars", type: :request do
-
-end
