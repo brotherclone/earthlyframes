ad883f4ce2da245df64ac533f1088c263009d95f
Gabriel Walsh <brotherclone@gmail.com>
2020-12-11 09:41:49 -0500

    this closes #63, this closes #64, this closes #62, this closes #54, this closes #55

--- app/controllers/rainbow_table_controller.rb
+++ app/controllers/rainbow_table_controller.rb
@@ -4,7 +4,21 @@ class RainbowTableController < ApplicationController
     add_breadcrumb "Home", :root_path
     add_breadcrumb "Albums", :albums_path
     add_breadcrumb "The Rainbow Table", :rainbow_path
-    @rainbow_albums = Album.where.not(:rainbow_table => 'not_associated').order(:rainbow_table)
-  end
+    @rainbow_table_albums  = Album.where.not(:rainbow_table => 'not_associated').order(:rainbow_table)
+    @unfinished_albums = []
 
+    Album.rainbow_tables.each do |c|
+      if c[1] != 0
+        check = false
+        @rainbow_table_albums.each do |album|
+          if album.rainbow_table == c[0]
+            check = true
+          end
+        end
+        unless check
+          @unfinished_albums << {:color => c[0], :color_id => c[1] }
+        end
+      end
+    end
+  end
 end
