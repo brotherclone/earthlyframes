2600a9532d1f69da44614db2bb3d0a19b6df2919
Gabriel Walsh <brotherclone@gmail.com>
2021-08-19 15:18:09 -0400

    removal of pulsar types

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  has_many :characters
-
-end
--- spec/factories/character_roles.rb
+++
@@ -1,5 +0,0 @@
-FactoryBot.define do
-  factory :character_role do
-    character_role {Faker::Lorem.paragraph}
-  end
-end
