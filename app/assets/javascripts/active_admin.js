e40ceeb9dc9107fc9350290e7bae65966c1d7278
Gabriel Walsh <brotherclone@gmail.com>
2022-08-11 12:26:17 -0400

    this closes #148

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -4,7 +4,10 @@
 $(document).ready(function() {
     tinymce.init({
         selector: 'textarea',
-        height: 600
+        height: 600,
+        plugins: 'link',
+        menubar: 'insert',
+        toolbar: ['undo redo | styleselect | bold italic | link | alignleft aligncenter alignright']
     })
 });
 
