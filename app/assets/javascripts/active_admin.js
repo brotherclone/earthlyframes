5000c2e05581042a939a3ca469b95503481673c0
Gabriel Walsh <brotherclone@gmail.com>
2024-04-04 15:44:29 -0400

    remove tinymce

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,13 +1,1 @@
 //= require active_admin/base
-//= require tinymce
-
-$(document).ready(function() {
-    tinymce.init({
-        selector: 'textarea',
-        height: 600,
-        plugins: 'codesample',
-        toolbar: 'codesample',
-        codesample_global_prismjs: true
-    })
-});
-
