24b46d5e4eada9835be39ea43856843a127eb9f0
Gabriel Walsh <brotherclone@gmail.com>
2024-05-05 17:40:11 -0400

    this closes #192

--- app/javascript/controllers/ratio_controller.js
+++ app/javascript/controllers/ratio_controller.js
@@ -1,18 +1,17 @@
-import { Controller } from "@hotwired/stimulus"
-import { useWindowResize } from 'stimulus-use'
+import {Controller} from "@hotwired/stimulus"
+import {useWindowResize} from 'stimulus-use'
 
 export default class extends Controller {
 
-    static targets = [ "embed"]
+    static targets = ["embed"]
 
-    connect(){
+    connect() {
         useWindowResize(this)
     }
 
-    windowResize({ width }) {
-        console.log(this.embedTarget)
+    windowResize({width}) {
         this.embedTarget.width = width
-        this.embedTarget.height = Math.floor(width/1.78)
+        this.embedTarget.height = Math.floor(width / 1.78)
     }
 
 }
